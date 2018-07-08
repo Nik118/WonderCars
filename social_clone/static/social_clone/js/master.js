b5f1d0f489a8c61d060a91235425a1066ec0c7a5
Hegde <nikhilx.hegde@intel.com>
2018-07-08 17:08:52 +0530

    wrong repository

--- social_clone/static/social_clone/js/master.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-  var cars = document.querySelectorAll("#cars img"), i = 1;
-  Array.prototype.forEach.call(cars, function(car) {
-	setTimeout(function(){ car.classList.add("visible") }, 700*i)
-	i++;
-})
-};
